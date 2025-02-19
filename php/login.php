3ba584a80ed61ed5b72b1d96eb2a93c0e4bdfb83
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2025-02-19 10:54:41 +0100

    Connexion et déconnexion

--- php/login.php
+++ php/login.php
@@ -1,5 +1,4 @@
 <?php
-//session_start(); // Démarrer la session
 require 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
--- php/user_add.php
+++ php/user_add.php
@@ -42,9 +42,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                             class="fas fa-cogs mr-3"></i> Mon compte</a></li>
 
         <div class="mt-6">
-            <button onclick="logout()" class="text-xl w-full bg-red-600 hover:bg-red-700 text-white py-5 rounded-lg shadow-md">
-                Déconnexion
-            </button>
+        <button onclick="logout()" ><a href="login.php" class="text-xl w-full bg-red-600 hover:bg-red-700 text-white py-5 rounded-lg shadow-md">
+         Déconnexion </a></button>
         </div>
     </div>
 
